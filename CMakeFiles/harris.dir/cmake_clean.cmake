24f66f3214455a0c3efda67b5dbda212585a1801
Aranya Dan <aranya.dan1996@gmail.com>
2015-12-07 00:26:16 +0530

    Created repo. Currently only with  program for harris squares.

---
+++ CMakeFiles/harris.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/harris.dir/src/harriscorner.cpp.o"
+  "bin/harris.pdb"
+  "bin/harris"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/harris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
